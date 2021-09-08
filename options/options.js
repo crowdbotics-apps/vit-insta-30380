76628439ded478e9ed63741de7038bd7e06af87a
Crowdbotics <team@crowdbotics.com>
2021-09-08 01:22:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "vit_insta_30380Identifier",
+  url: "https://vit_insta_30380Identifier.botics.co",
+  api: "https://vit_insta_30380Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
